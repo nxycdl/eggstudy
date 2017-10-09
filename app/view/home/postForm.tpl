7e82fd16533ad18e62505ed4e29b22c13f9a24a3
nxycdl <nxycdl@163.com>
2017-10-09 16:53:36 +0800

    commit

--- app/view/home/postForm.tpl
+++ app/view/home/postForm.tpl
@@ -12,14 +12,17 @@
 <div>
     <center>
         <form action="http://localhost:7001/postFormSub" method="post">
+            <input type="hidden" name="_csrf" value="{{ ctx.csrf }}">
             <p>用户名：<input type="text" name="name" value="{{username}}"/></p>
             <p>密码：<input type="password" name="pwd"/></p>
+            <label>这里有一个隐藏的_csrf,在PostForm里面可以有，也可以没有。如果没有框架会自动默认post一个上去;</label>
             <input type="submit" value="登录">
         </form>
     </center>
 </div>
 </body>
 <script>
-
+    var list = {{list|dump|safe}};
+    console.log(list);
 </script>
 </html>
--- config/config.default.js
+++ config/config.default.js
@@ -1,60 +1,59 @@
 'use strict';
-
 module.exports = appInfo => {
-    const config = exports = {};
+    const config = exports = {
 
-    // use for cookie sign key, should change to your own and keep security
-    config.keys = appInfo.name + '_1507377681959_2214';
-    // config.keys = 'egg_1507377681959_2214';
+        // use for cookie sign key, should change to your own and keep security
+        keys: appInfo.name + '_1507377681959_2214',
+        // config.keys : 'egg_1507377681959_2214',
 
-    // add your config here
-    config.middleware = [];
+        // add your config here
+        middleware: [],
 
-    // add view config
-    exports.view = {
-        defaultViewEngine: 'nunjucks',
-        mapping: {
-            '.tpl': 'nunjucks',
+        // add view config
+        view: {
+            defaultViewEngine: 'nunjucks',
+            mapping: {
+                '.tpl': 'nunjucks',
+            },
         },
-    };
 
-    exports.news = {
-        pageSize: 5,
-        serverUrl: 'https://cnodejs.org/api/v1',
-        serverTopic: 'http://cnodejs.org/topic'
-    };
+        news: {
+            pageSize: 5,
+            serverUrl: 'https://cnodejs.org/api/v1',
+            serverTopic: 'http://cnodejs.org/topic'
+        },
 
-    exports.security = {
-        csrf: {
-            // 判断是否需要 ignore 的方法，请求上下文 context 作为第一个参数
-            ignore: '/postDemo',
-            queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
-            bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
+        security: {
+            csrf: {
+                // 判断是否需要 ignore 的方法，请求上下文 context 作为第一个参数
+                ignore: '/postDemo',
+                queryName: '_csrf', // 通过 query 传递 CSRF token 的默认字段为 _csrf
+                bodyName: '_csrf', // 通过 body 传递 CSRF token 的默认字段为 _csrf
+            },
         },
-    };
-    // 是否启用CSRF;
-    /* exports.security = {
-     csrf: false
-     }; */
-    exports.mysql = {
-        // 单数据库信息配置
-        client: {
-            // host
-            host: '120.26.109.153',
-            // 端口号
-            port: '3306',
-            // 用户名
-            user: 'btc',
-            // 密码
-            password: 'btc99999',
-            // 数据库名
-            database: 'btc',
-        },
-        // 是否加载到 app 上，默认开启
-        app: true,
-        // 是否加载到 agent 上，默认关闭
-        agent: false,
+        // 是否启用CSRF,
+        /* exports.security : {
+         csrf: false
+         }, */
+        mysql: {
+            // 单数据库信息配置
+            client: {
+                // host
+                host: '120.26.109.153',
+                // 端口号
+                port: '3306',
+                // 用户名
+                user: 'btc',
+                // 密码
+                password: 'btc99999',
+                // 数据库名
+                database: 'btc',
+            },
+            // 是否加载到 app 上，默认开启
+            app: true,
+            // 是否加载到 agent 上，默认关闭
+            agent: false,
+        }
     }
-
     return config;
-};
+}
